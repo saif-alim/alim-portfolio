8dcdf3ba5b61e28415f7fc72a62958966079da8d
Saif Ul Alim Ahmed <saifulalim2002@gmail.com>
2025-10-20 08:26:17 +0100

    set ups

---
+++ src/sections/HeroSection.tsx
@@ -0,0 +1,13 @@
+export const HeroSection = () => {
+  return (
+    <>
+      <div className="h-screen flex flex-row">
+        <div className="flex-1">placeholder</div>
+        <div className="flex flex-1 flex-col">
+          <h1 className="text-5xl font-bold">Hi, I'm Alim</h1>
+          <h2 className="text-2xl">I'm a software developer</h2>
+        </div>
+      </div>
+    </>
+  );
+};

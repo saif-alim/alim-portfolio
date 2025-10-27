7d1e0d86e5d78b4ff2864f8e1871359421769e3e
Saif Ul Alim Ahmed <saifulalim2002@gmail.com>
2025-10-27 13:13:15 +0000

    Update SectionTitle.tsx

--- src/components/custom/SectionTitle.tsx
+++ src/components/custom/SectionTitle.tsx
@@ -4,7 +4,7 @@ import type { ReactNode } from "react";
 export const SectionTitle: React.FC<{ children: ReactNode }> = ({
   children,
 }) => (
-  <h1 className="font-medium text-left tracking-widest text-3xl md:text-4xl leading-none m-0 pb-10">
+  <h2 className="font-medium text-left tracking-widest text-3xl md:text-4xl leading-none m-0 pb-10">
     {children}
-  </h1>
+  </h2>
 );

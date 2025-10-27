3d9034cee7a07a0c30efa9b372bcd6f2f467ba77
Saif Ul Alim Ahmed <saifulalim2002@gmail.com>
2025-10-27 15:11:04 +0000

    Update Footer.tsx

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -1,8 +1,12 @@
 export const Footer = () => (
   <div className="max-w-5xl mx-auto px-[5%]">
     <hr />
-    <footer className="w-full py-6 text-center text-sm text-gray-500">
-      &copy; {new Date().getFullYear()} Saif Ul Alim Ahmed. All rights reserved.
+    <footer className="flex flex-col w-full py-6 text-center text-sm text-gray-500">
+      &copy; {new Date().getFullYear()} Saif Ul Alim Ahmed — All rights
+      reserved.
+      <span className="text-xs font-light py-1">
+        Built with React, TypeScript &amp; Tailwind CSS.
+      </span>
     </footer>
   </div>
 );
